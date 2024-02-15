e285bd8c93d8b18b0aa8caaf957a5c39b65ea74e
S78648 <sirohisumit4@gmail.com>
2024-02-15 16:00:13 +0530

    setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import env from 'dotenv'
+import mongoose from 'mongoose'
 
 env.config();
 const app = express();
-
 app.use(express.json());
+
+
 
 app.listen(process.env.PORT,()=>console.log(`App running on ${process.env.PORT}`));
